ba98c66cef1cf1c6ee6d51adfb676259c83f7b31
Eline Madsø <elinemadso@gmail.com>
2024-03-18 19:06:16 +0100

    navigasjonsbar fungerer

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import "./Navbarstyling.css";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="navbar-container">
-        <div className="myName">
-          <Link to="personal-webpage/">Eline Madsø</Link>
-        </div>
-
-        <ul>
+        <ul className="nav-links">
           <li>
             <Link to="personal-webpage/gallery">Painting gallery</Link>
           </li>
+
+          <li className="myName">
+            <Link to="personal-webpage/">Eline Madsø</Link>
+          </li>
+
           <li>
             <Link to="personal-webpage/projects">Projects</Link>
           </li>
