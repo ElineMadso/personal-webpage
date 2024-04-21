0018cc3c00414075491e6408937597b7ef3d54a0
Eline Madsø <elinemadso@gmail.com>
2024-04-21 20:34:31 +0200

    la til eksempel-bilde på forsiden

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,20 +5,29 @@ const HomePage = () => {
     <>
       <div className={"grid-container"}>
         <div className={"section section1"}>
-          <p className={"intro-1"}>Hi, I'm Eline Madsø,</p>
-          <h1 className={"intro-2"}>A freelance UX / interaction Designer</h1>
-          <button className={"get-in-touch-button"}>Get in Touch</button>
+          <div className={"left-content"}>
+            <p className={"intro-1"}>Hi, I'm Eline Madsø,</p>
+            <h1 className={"intro-2"}>A freelance UX / interaction Designer</h1>
+            <button className={"get-in-touch-button"}>Get in Touch</button>
+          </div>
+          <div className="right-content">
+            <img src="./images/image-of-me.jpg" alt="your Image" className="image1"/>
+          </div>
         </div>
+
         <div className={"section section2"}>
           <h2 className={"headline"}>headline</h2>
         </div>
+
         <div className={"section section3"}>
           <p className={"words-about-me"}>Ord om meg</p>
           <button className={"about-me-button"}>About me</button>
         </div>
+
         <div className={"section section4"}>
           <p className={"offer-text"}>What can I do?</p>
         </div>
+
         <div className={"area banner"}></div>
       </div>
     </>
